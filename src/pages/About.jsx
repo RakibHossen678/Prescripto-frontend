415ffdfaf85c95b54b316a089d4830fe3d8bb810
RakibHossen678 <rh491464@gmail.com>
2024-09-25 18:02:48 +0600

    added contact page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -21,9 +21,9 @@ const About = () => {
             Prescripto is committed to excellence in healthcare technology. We
             continuously strive to enhance our platform, integrating the latest
             advancements to improve user experience and deliver superior
-            service. Whether you're booking your first appointment or managing
-            ongoing care, Prescripto is here to support you every step of the
-            way.
+            service. Whether you{"'"}re booking your first appointment or
+            managing ongoing care, Prescripto is here to support you every step
+            of the way.
           </p>
           <b className="text-gray-800">Our Vision</b>
           <p>
